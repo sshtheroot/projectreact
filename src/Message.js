05661806c8b3293e7e450f63ee32b34ec3499af6
bengalurudev <bengalurudev23@gmail.com>
2024-01-30 23:42:33 +0530

    get api

--- src/Message.js
+++ src/Message.js
@@ -1,25 +1,53 @@
-import React, { useState } from 'react';
+ 
+
+
+
+import React, { useState, useEffect } from 'react';
 
 function Message() {
   const [data, setData] = useState(null);
 
-  function handleClick() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://cloud-wor7.onrender.com/product/1');
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        setData(JSON.parse(xhr.responseText));
-      }
-    };
-    xhr.send();
-  }
+  useEffect(() => {
+    fetch('https://cloud-wor7.onrender.com/product/1')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }, []);
 
   return (
     <div>
-      <button onClick={handleClick}>Get Data</button>
-      {data ? <div>{JSON.stringify(data)}</div> : <div>Loading...</div>}
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
     </div>
   );
 }
 
 export default Message;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
