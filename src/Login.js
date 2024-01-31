b1506cb5e3759975f33d5e79f3505cda37094187
bengalurudev <bengalurudev23@gmail.com>
2024-01-31 14:47:28 +0530

    #

--- src/Login.js
+++ src/Login.js
@@ -1,26 +1,25 @@
 
 
-function login(){
+function Login() {
 
-    return (
-<form>
- 
-  <div class="form-outline mb-4">
-    <input type="username" id="form2Example1" class="form-control" />
-    <label class="form-label" for="form2Example1">Username</label>
-  </div>
- 
-  <div class="form-outline mb-4">
-    <input type="password" id="form2Example2" class="form-control" />
-    <label class="form-label" for="form2Example2">Password</label>
-  </div>
- 
-  <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
- 
-</form>
+  return (
+    <form>
 
-    );
+      <div class="form-outline mb-4">
+        <input type="username" id="form2Example1" class="form-control" />
+        <label class="form-label" for="form2Example1">Username</label>
+      </div>
 
+      <div class="form-outline mb-4">
+        <input type="password" id="form2Example2" class="form-control" />
+        <label class="form-label" for="form2Example2">Password</label>
+      </div>
 
+      <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
 
+    </form>
+
+  );
 }
+
+export default Login;
