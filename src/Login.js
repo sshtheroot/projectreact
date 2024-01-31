918a27bd3b570ea8b77b7730117a7f61cc0011b9
bengalurudev <bengalurudev23@gmail.com>
2024-01-31 18:20:41 +0530

    #

--- src/Login.js
+++ src/Login.js
@@ -15,7 +15,7 @@ function Login() {
   return (
 
     
-    <form >
+    <form onSubmit={check}>
 
       <div class="form-outline mb-4">
         <input name= "username" id="form2Example1" class="form-control" />
@@ -27,7 +27,7 @@ function Login() {
         <label class="form-label" for="form2Example2">Password</label>
       </div>
 
-      <button onSubmit={check} type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
+      <button  type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
 
     </form>
 
