46c84c40b1ae146d914aebaeffc7795ca04d16f2
bengalurudev <bengalurudev23@gmail.com>
2024-01-31 11:04:50 +0530

    app

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Message from './Message';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Message />
+    <Message /> <App/>
   </React.StrictMode>,
   document.getElementById('root')
  
