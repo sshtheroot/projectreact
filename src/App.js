30538a15d3ade71ddfa8591134a639975867596f
bengalurudev <bengalurudev23@gmail.com>
2024-01-31 18:18:10 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './Login';
+
 function App() {
   return (
     <div className="App">
@@ -16,7 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           
-     <Login/>
+     
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Message from './Message';
 import App from './App';
-
+import Login from './Login';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Message />  <App/> 
+    <Message />  <App/> <Login/>
   </React.StrictMode>,
   document.getElementById('root')
  
 );
  
+
 
 reportWebVitals(sendToVercelAnalytics);
