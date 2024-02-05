b0a527695b2ba0a666e0b06bb1b75de144304bd4
bengalurudev <bengalurudev23@gmail.com>
2024-02-06 00:37:25 +0530

    Pic

--- src/Profile.js
+++ src/Profile.js
@@ -6,7 +6,7 @@ function Profile() {
   return (
     <Card style={{ width: '18rem' }}>
      
-      <Card.Img variant="top" src="https://fastly.picsum.photos/id/8/200/200.jpg?hmac=OeG5ufhPYQBd6Rx1TAldAuF92lhCzAhKQKttGfawWuA" roundedCircle/> 
+      <Card.Img variant="top" src="https://fastly.picsum.photos/id/8/200/200.jpg?hmac=OeG5ufhPYQBd6Rx1TAldAuF92lhCzAhKQKttGfawWuA"/> 
       <Card.Body>
         <Card.Title>Bengaluru Developer</Card.Title>
         <Card.Text>
