693f4f772a9f8f0e895fcf8806cd958066a1c014
bengalurudev <bengalurudev23@gmail.com>
2024-02-06 01:03:03 +0530

    #

--- src/ListArticle.js
+++ src/ListArticle.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { Stack } from 'react-bootstrap';
 
 function ListArticle() {
 
@@ -27,20 +26,14 @@ function ListArticle() {
             {
                 name.map((data) => {
 
-                    return (/* 
+                    return (
                         <ul className="list group list-group-named">
                             <li className="list-group=item"> {data.title}</li>
                             <p> <a href={data.url}>{data.url}</a> </p>
 
 
 
-                        </ul> */
-<ul className="list group list-group-named">
-                        <Stack gap={3}>
-                        <div className="p-2">{data.title}</div>
-                        <div className="p-2">{data.url}</div> 
-                      </Stack>
-                      </ul>
+                        </ul>
                     )
                 })
             }
