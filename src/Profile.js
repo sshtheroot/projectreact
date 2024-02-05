fd27e6841b48c4ecf0761b8fff701b33f43d663a
bengalurudev <bengalurudev23@gmail.com>
2024-02-06 00:04:31 +0530

    #

--- src/Profile.js
+++ src/Profile.js
@@ -4,7 +4,8 @@ import Card from 'react-bootstrap/Card';
 function Profile() {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://fastly.picsum.photos/id/8/5000/3333.jpg?hmac=OeG5ufhPYQBd6Rx1TAldAuF92lhCzAhKQKttGfawWuA" />
+      <Col xs={6} md={4}>
+      <Card.Img variant="top" src="https://fastly.picsum.photos/id/8/5000/3333.jpg?hmac=OeG5ufhPYQBd6Rx1TAldAuF92lhCzAhKQKttGfawWuA" roundedCircle/></Col>
       <Card.Body>
         <Card.Title>Bengaluru Developer</Card.Title>
         <Card.Text>
