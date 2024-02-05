4f1114f0fb65bd15b3514865e2c1abcfce163d50
bengalurudev <bengalurudev23@gmail.com>
2024-02-05 17:34:59 +0530

    ListArticle

--- src/ListArticle.js
+++ src/ListArticle.js
@@ -1,15 +1,34 @@
 
 
-const data =  
-    fetch('https://crawler-micro.onrender.com/Amazon')
-      .then(response => response.json())
-      
-      .catch(error => console.error(error));
- 
-
-export default function List() {
-    const listItems = data.map(data =>
-      <li>{data}</li>
-    );
-    return <ul>{listItems}</ul>;
+class MyComponent extends React.Component {
+    constructor() {
+      super()
+      this.state = { movies: [] }
+    }
+  
+    ListArticle() {
+      var myRequest = new Request('https://crawler-micro.onrender.com/Amazon');
+      let movies = [];
+  
+      fetch(myRequest)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({ movies: data })
+        })
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>Movie List</h1>
+          <ul>
+            {this.state.movies.map(movie => {
+              return <li key={`movie-${movie.id}`}>{movie.name}</li>
+            })}
+          </ul>
+        </div>
+      )
+    }
   }
+
+  export default ListArticle;
