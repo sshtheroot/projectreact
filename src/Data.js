9a24ddb6020cd84d4a569c023d0111c8b1e0f184
bengalurudev <bengalurudev23@gmail.com>
2024-02-05 16:58:46 +0530

    rollback

--- src/Data.js
+++ src/Data.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect, useParams } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Data() {
-  const [data, setData] = useState(null);
-  const { id } = useParams();
+  const [data, setData] = useState(null); 
 
   useEffect(() => {
     fetch('https://crawler-micro.onrender.com/${id}')
@@ -13,10 +12,11 @@ function Data() {
 
   return (
     <div>
-        <Input type="text" name="name" id="name"  />
+     
       {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'More Data'}
     </div>
   );
 }
 
 export default Data;
+ 
