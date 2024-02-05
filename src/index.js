2cc864f8f8500c168cf4a167d97af31a9dbb752e
bengalurudev <bengalurudev23@gmail.com>
2024-02-05 11:36:06 +0530

    #

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Message />  <App />  
+ <App />  
   </React.StrictMode>,
   document.getElementById('root')
  
