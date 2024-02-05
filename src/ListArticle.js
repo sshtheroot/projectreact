3981e7cf56c3020f7e05ccb2879f3d37e64bf38d
bengalurudev <bengalurudev23@gmail.com>
2024-02-05 17:56:24 +0530

    #

--- src/ListArticle.js
+++ src/ListArticle.js
@@ -1,37 +1,43 @@
+import {useState, useEffect} from 'react';
+
+function ListArticle(){
 
 
-import React from 'react'; 
+const[name,setName] = useState([]);
 
 
-class ListArticle extends React.Component {
-    constructor() {
-      super()
-      this.state = { movies: [] }
-    }
-  
-    cool() {
-      var myRequest = new Request('https://crawler-micro.onrender.com/Amazon');
-      let movies = [];
-  
-      fetch(myRequest)
-        .then(response => response.json())
-        .then(data => {
-          this.setState({ movies: data })
-        })
-    }
-  
-    render() {
-      return (
-        <div>
-          <h1>Movie List</h1>
-          <ul>
-            {this.state.movies.map(movie => {
-              return <li key={`movie-${movie.title}`}>{movie.url}</li>
-            })}
-          </ul>
-        </div>
-      )
-    }
-  }
+useEffect(()=> {
+    names()
+}, [])
 
-  export default ListArticle;
+
+const names= async() => {
+
+    const response= await fetch('https://crawler-micro.onrender.com/Amazon');
+
+    setName(await response.json())
+}
+
+return (
+
+    <div>
+
+        <ol className="list group list-group-named">
+        {
+            name.map((data) => {
+
+                return (
+                    <li className = "list-group=item" key ={data.title} > { data.url}</li>
+                )
+            })
+        }
+
+        </ol>
+    </div>
+)
+
+
+
+}
+
+export default ListArticle;
