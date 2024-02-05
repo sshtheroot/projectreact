5d6337256e522fce6344a4b3f86bbc86bf616336
bengalurudev <bengalurudev23@gmail.com>
2024-02-05 23:06:28 +0530

    #

--- src/Profile.js
+++ src/Profile.js
@@ -6,10 +6,10 @@ function Profile() {
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Bengaluru Dev</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          Software Developer
+          Cloud Technology Enthusiast
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
