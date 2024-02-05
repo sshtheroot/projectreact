65549d62c76ef2f99359171f6dd362711e75f327
bengalurudev <bengalurudev23@gmail.com>
2024-02-05 11:19:05 +0530

    #

--- src/Message.js
+++ src/Message.js
@@ -22,9 +22,9 @@ function Message() {
     <div className="container">     
     <h1> Kist of articles </h1>  
  
-    {JSON.map(title, url => (  
+    {JSON.map(title => (  
       <li>  
-        {title}  
+        {title.title}  
       </li>  
      
     ))}  
