cd299abe7a515b1ca6d9cf39da489a9945b9a3f4
bengalurudev <bengalurudev23@gmail.com>
2024-02-05 19:10:11 +0530

    #

--- src/Profile.js
+++ src/Profile.js
@@ -15,33 +15,13 @@ export default function Profile() {
                   <div className="flex-shrink-0">
                     <MDBCardImage
                       style={{ width: '180px', borderRadius: '10px' }}
-                      src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp'
+                      src='https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU'
                       alt='Generic placeholder image'
                       fluid />
                   </div>
                   <div className="flex-grow-1 ms-3">
                     <MDBCardTitle>Bengaluru Dev</MDBCardTitle>
-                    <MDBCardText>Software Developer</MDBCardText>
-
-                    <div className="d-flex justify-content-start rounded-3 p-2 mb-2"
-                      style={{ backgroundColor: '#efefef' }}>
-                      <div>
-                        <p className="small text-muted mb-1">Articles</p>
-                        <p className="mb-0">41</p>
-                      </div>
-                      <div className="px-3">
-                        <p className="small text-muted mb-1">Followers</p>
-                        <p className="mb-0">976</p>
-                      </div>
-                      <div>
-                        <p className="small text-muted mb-1">Rating</p>
-                        <p className="mb-0">8.5</p>
-                      </div>
-                    </div>
-                    <div className="d-flex pt-1">
-                      <MDBBtn outline className="me-1 flex-grow-1">Chat</MDBBtn>
-                      <MDBBtn className="flex-grow-1">Follow</MDBBtn>
-                    </div>
+                    <MDBCardText>Software Developer</MDBCardText> 
                   </div>
                 </div>
               </MDBCardBody>
