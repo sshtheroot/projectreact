9e6d73cc98c791c9d82da61f0199d65758f98f6d
bengalurudev <bengalurudev23@gmail.com>
2024-02-05 18:28:55 +0530

    #

--- src/ListArticle.js
+++ src/ListArticle.js
@@ -29,7 +29,7 @@ return (
                 return (
                     <ul className="list group list-group-named">
                     <li className = "list-group=item"> { data.title}</li>
-                    <li className = "list-group=item"> <a href={data.url}>{data.url}</a> </li>
+                    <p> <a href={data.url}>{data.url}</a> </p>
                 
         </ul>
                 )
