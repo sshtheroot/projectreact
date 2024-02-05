cb7e6703316465702020e5fb016cf2328717a1a7
bengalurudev <bengalurudev23@gmail.com>
2024-02-05 16:49:43 +0530

    #

--- src/Data.js
+++ src/Data.js
@@ -4,14 +4,15 @@ function Data() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch('https://crawler-micro.onrender.com/amazon')
+    fetch('https://crawler-micro.onrender.com/${id}')
       .then(response => response.json())
       .then(json => setData(json))
       .catch(error => console.error(error));
-  }, []);
+  }, [id]);
 
   return (
     <div>
+        <Input type="text" name="name" id="name"  />
       {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'More Data'}
     </div>
   );
