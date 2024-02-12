ccbbc3020c9b49163bd699be32d8e40f3ad527b6
bengalurudev <bengalurudev23@gmail.com>
2024-02-13 00:38:00 +0530

    #

--- src/Test.js
+++ src/Test.js
@@ -4,11 +4,13 @@ import React from 'react';
 function Test() {
 
 
+    const name='rahul';
+
     return ( <div>
 
-        <h1> Welcome to React Learning Component</h1> 
+        <h1> Welcome to React Learning Component, {name} </h1> 
 
-    </div>)
+    </div>);
 }
 
 export default Test;
