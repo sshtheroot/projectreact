44d1fadad6336cfcade48f5f997698abe443321f
bengalurudev <bengalurudev23@gmail.com>
2024-02-13 17:28:56 +0530

    Test

--- src/Test.js
+++ src/Test.js
@@ -6,11 +6,20 @@ function Test() {
 
     const name='rahul';
 
+    const title= React.createElement('h1',null,'Hello from ProjectReact');
+    const link=React.createElement('a', {href:'www.google.com'},'Read More');
+    const group= React.createElement('div',null,title,link);
+    const domElement=document.getElementById('root');
+
+    ReactDOM.render(group,domElement);
+
     return ( <div>
 
         <h1> Welcome to React Learning Component, {name} </h1> 
 
     </div>);
 }
+
+
 
 export default Test;
