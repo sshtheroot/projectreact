5eeb65d283105cef86166b6a71fd264461def25a
bengalurudev <bengalurudev23@gmail.com>
2024-02-22 14:27:52 +0530

    #

--- src/Data.js
+++ src/Data.js
@@ -1,7 +1,7 @@
  
- 
- import React, { useState, useEffect } from 'react';
  import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+ 
  
  const Data = () => {
    const [currentPage, setCurrentPage] = useState(1);
