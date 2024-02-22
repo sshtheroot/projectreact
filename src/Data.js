7983536a8a3a3713428939e58518c3ea55999ba3
bengalurudev <bengalurudev23@gmail.com>
2024-02-22 13:21:57 +0530

    #

--- src/Data.js
+++ src/Data.js
@@ -1,21 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
 function Data() {
-  const [data, setData] = useState(null); 
-
+  const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
 
-
   useEffect(() => {
-  
-    fetch('https://crawler-micro.onrender.com/page/${currentPage}/size/${itemsPerPage}')
+    fetch(`https://crawler-micro.onrender.com/page/${currentPage}/size/${itemsPerPage}`)
       .then(response => response.json())
-      .then(json => setData(json))
+      .then(data => setData(data))
       .catch(error => console.error(error));
   }, [currentPage, itemsPerPage]);
-
-
 
   const totalPages = Math.ceil(data.length / itemsPerPage);
   const startIndex = (currentPage - 1) * itemsPerPage;
@@ -23,19 +18,20 @@ function Data() {
   const currentItems = data.slice(startIndex, endIndex);
 
   return (
-    <div> 
-<h1>List of the articles</h1>
-{currentItems.map(item => (
+    <div>
+      // Render paginated data
+      <h1>List of Articles</h1>
+      {currentItems.map(item => (
         <div key={item.id}>
           <p>{item.title}</p>
           <p>{item.url}</p>
         </div>
       ))}
+      // Render pagination controls
     </div>
   );
-
- 
 }
 
 export default Data;
+
  
