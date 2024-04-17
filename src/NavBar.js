28a9df18ca599026060c79c397b51561988aaa87
sshtheroot <sshtheroot@gmail.com>
2024-04-17 09:49:16 +0530

    #

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,6 @@
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown'; 
-import Theme from './Theme';
-import siteMetadata from '@/data/siteMetadata'
-import headerNavLinks from '@/data/headerNavLinks'
+import Theme from './Theme'; 
 
 
 function NavBar() {
@@ -14,8 +12,7 @@ function NavBar() {
   
   return (
     
-    
-    <> 
+ 
     <header className="flex items-center justify-between py-10">
     <div className="hidden h-6 text-2xl font-semibold sm:block">
                 Bengaluru Dev
@@ -41,7 +38,7 @@ function NavBar() {
 
       </Nav>
       </header>
-    </>
+   
   );
 }
 
