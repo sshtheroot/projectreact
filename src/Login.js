9f58334813b8e051fd1c5634562f55ff54ab92c4
sshtheroot <158087633+sshtheroot@users.noreply.github.com>
2024-10-30 22:08:43 +0530

    fff

--- src/Login.js
+++ src/Login.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
+    const [userName, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('https://92a0-49-207-193-14.ngrok-free.app/login', { username, password });
+            const response = await axios.post('https://92a0-49-207-193-14.ngrok-free.app/login', { userName, password });
           
             alert('Login successful');
         } catch (err) {
@@ -20,7 +20,7 @@ const Login = () => {
     return (
         <div>
             <form onSubmit={handleLogin}>
-                <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="text" placeholder="Username" value={userName} onChange={(e) => setUsername(e.target.value)} />
                 <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <button type="submit">Login</button>
             </form>
