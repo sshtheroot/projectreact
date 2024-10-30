d5a8649b3a236ee81552c72e11f03827404e412a
sshtheroot <158087633+sshtheroot@users.noreply.github.com>
2024-10-30 18:27:36 +0530

    #

--- src/Login.js
+++ src/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('https://3fec-49-207-193-14.ngrok-free.app/login', { username, password });
+            const response = await axios.post('https://fb8d-49-207-193-14.ngrok-free.app/login', { username, password });
             localStorage.setItem('token', response.data.token);
             alert('Login successful');
         } catch (err) {
